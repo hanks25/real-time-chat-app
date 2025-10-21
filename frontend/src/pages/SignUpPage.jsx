b23108313cab23c8338bdb19a2f75e77f1bfc77c
hanks <chenhanks@gmail.com>
2025-10-20 22:27:29 -0400

    adding button for create account in signup page

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -103,6 +103,20 @@ export const SignUpPage = () => {
                 </button>
               </div>
             </div>
+            <button
+              type="submit"
+              className="btn btn-primary w-full"
+              disabled={isSigningUp}
+            >
+              {isSigningUp ? (
+                <>
+                  <Loader2 className="size-5 animate-spin" />
+                  Loading...
+                </>
+              ) : (
+                "Create Account"
+              )}
+            </button>
           </form>
         </div>
       </div>

6c1a2c2fc26e2aadf3d09585ffd94d76c7240c8a
hanks <chenhanks@gmail.com>
2025-11-12 15:59:37 -0500

    adding theme color change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,20 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import { Routes, Route, Navigate } from "react-router-dom";
 
-import HomePage from "./pages/HomePage";
-import SignUpPage from "./pages/SignUpPage";
-import LoginPage from "./pages/LoginPage";
-import SettingPage from "./pages/SettingPage";
-import ProfilePage from "./pages/ProfilePage";
-import { useAuthStore } from "./store/useAuthStore";
+import { Loader } from "lucide-react";
 import { useEffect } from "react";
-import {Loader} from "lucide-react";
 import { Toaster } from "react-hot-toast";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import SettingPage from "./pages/SettingPage";
+import SignUpPage from "./pages/SignUpPage";
+import { useAuthStore } from "./store/useAuthStore";
+import { useThemeStore } from "./store/useThemeStore";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  const { theme } = useThemeStore()
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
@@ -27,7 +29,7 @@ const App = () => {
     );
 
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
       <Routes>
         <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />

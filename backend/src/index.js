3f1e885acaea5a4d113526da44e5dd11ac2a6dd9
hanks <chenhanks@gmail.com>
2025-11-16 23:02:32 -0500

    finishing up the project and prepare for deploying

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,11 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
-
+import path from "path";
 import { connectDB } from "./lib/db.js";
 import { app, server } from "./lib/socket.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 
@@ -20,10 +20,17 @@ app.use(
   })
 );
 const PORT = process.env.PORT;
-
+const __dirname = path.resolve();
 app.use("/api/auth", authRoutes);
-
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  })
+}
 
 server.listen(PORT, () => {
   console.log("server is running PORT:" + PORT);
